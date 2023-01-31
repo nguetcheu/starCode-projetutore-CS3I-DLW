5989cb0a4223bc55266718654906ad52ddacd620
nguetcheu <dnguetcheu@gmail.com>
2023-01-31 23:21:38 +0100

    page blog ok

--- blog.php
+++ blog.php
@@ -49,30 +49,32 @@
       <div class="container">
         <div class="row">
 
-          <div class="col-md-8">
+          <div class="col-md-12">
             <div class="row mb-5">
               <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
                 <div class="h-entry">
-                  <img src="images/img_6.jpg" alt="Image" class="img-fluid">
-                  <h2 class="font-size-regular"><a href="#">Warehousing Your Packages</a></h2>
-                  <div class="meta mb-4">Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+                  <img src="images/website.jpg" alt="Image" class="img-fluid">
+                  <h2 class="font-size-regular"><a href="#">Créez un beau site Web en moins d'une heure</a></h2>
+                  <div class="meta mb-4">Ham Brook <span class="mx-2">&bullet;</span> Jan 18, 2022<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+                  <p>La création d'un site web grace a notte équipe expert peut se passer a un rythme incomparable canalisezvos idées et nous nous occuperons du reste.</p>
                 </div> 
               </div>
-              <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
-                <div class="h-entry">
-                  <img src="images/img_1.jpg" alt="Image" class="img-fluid">
-                  <h2 class="font-size-regular"><a href="#">Warehousing Your Packages</a></h2>
-                  <div class="meta mb-4">Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
-                </div>
+              <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+              <div class="h-entry">
+                <img src="images/rubiks.jpg" alt="Image" class="img-fluid" style="width: 300px;">
+                <h2 class="font-size-regular"><a href="#">Rubiks Cube plus qu'une image</a></h2>
+                <div class="meta mb-4">Sasha banks <span class="mx-2">&bullet;</span> Jan 01, 2023<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+                <p>Résoudre un Rubik's Cube d'une seule main est une tâche difficile, même pour les humains, et il faut plusieurs années aux enfants pour acquérir la dextérité nécessaire pour le maîtriser. Réaliser cet capture fut tellement satisfaisant intelligence humaine en un cliché</p>
+                <p><a href="#">Continue Reading...</a></p>
               </div>
-              <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
+            </div>
+              <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
                 <div class="h-entry">
                   <img src="images/img_3.jpg" alt="Image" class="img-fluid">
-                  <h2 class="font-size-regular"><a href="#">Warehousing Your Packages</a></h2>
-                  <div class="meta mb-4">Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+                  <h2 class="font-size-regular"><a href="#">C'est quoi une application mobile ?</a></h2>
+                  <div class="meta mb-4">Alexy morand <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+                  <p>Une application mobile est un programme qui se télécharge sur un smartphone ou une tablette, de façon gratuite ou payante. Il s'agit d'un logiciel développé exclusivement pour une utilisation dédiée sur un support mobile.</p>
+                  <p><a href="#">Continue Reading...</a></p>
                 </div> 
               </div>
               <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
@@ -100,51 +102,7 @@
                 </div>
               </div>
             </div>
-            <div class="row">
-              <div class="col-12">
-                <div class="custom-pagination text-center">
-                  <span>1</span>
-                  <a href="#">2</a>
-                  <a href="#">3</a>
-                  <span class="more-page">...</span>
-                  <a href="#">10</a>
-                </div>
-              </div>
-            </div>
           </div> 
-
-          <div class="col-md-3 ml-auto">
-            <div class="mb-5">
-              <h3 class="h5 text-black mb-3">Search</h3>
-              <form action="#" method="post">
-                <div class="form-group d-flex">
-                  <input type="text" class="form-control" placeholder="Search keyword and hit enter...">
-                </div>
-              </form>
-            </div>
-
-            <div class="mb-5">
-              <h3 class="h5 text-black mb-3">Popular Posts</h3>
-              <ul class="list-unstyled">
-                <li class="mb-2"><a href="#">Lorem ipsum dolor sit amet</a></li>
-                <li class="mb-2"><a href="#">Quaerat rerum voluptatibus veritatis</a></li>
-                <li class="mb-2"><a href="#">Maiores sapiente veritatis reprehenderit</a></li>
-                <li class="mb-2"><a href="#">Natus eligendi nobis</a></li>
-              </ul>
-            </div>
-
-            <div class="mb-5">
-              <h3 class="h5 text-black mb-3">Recent Comments</h3>
-              <ul class="list-unstyled">
-                <li class="mb-2"><a href="#">Joefrey</a> <em>in</em> <a href="#">Lorem ipsum dolor sit amet</a></li>
-                <li class="mb-2"><a href="#">Joefrey</a> <em>in</em> <a href="#">Quaerat rerum voluptatibus veritatis</a></li>
-                <li class="mb-2"><a href="#">Joefrey</a> <em>in</em> <a href="#">Maiores sapiente veritatis reprehenderit</a></li>
-                <li class="mb-2"><a href="#">Joefrey</a> <em>in</em> <a href="#">Natus eligendi nobis</a></li>
-              </ul>
-            </div>
-
-          </div>
-
         </div>
       </div>
     </section>
